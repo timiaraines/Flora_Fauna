3231d7d93246d2c48f6c4bf07c12c6bd7800fe54
timiaraines <72035201+timiaraines@users.noreply.github.com>
2022-10-26 09:29:32 -0500

    welcomeee

--- Flora_Fauna/WelcomeView.swift
+++ Flora_Fauna/WelcomeView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("Lotus-Flower")
+                .resizable()
+                .scaledToFit()
+                
+            Text("Welcome to")
+            Text("f l o r a   f a u n a")
+            Text("your one stop for all things women's health related")
+        }
     }
 }
+    
+    struct WelcomeView_Previews: PreviewProvider {
+        static var previews: some View {
+            WelcomeView()
+        }
+    }
 
-struct WelcomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        WelcomeView()
-    }
-}
