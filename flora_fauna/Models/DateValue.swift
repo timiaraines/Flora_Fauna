64c2bd452247490939e2d1032cf92ba30077bffa
timiaraines <72035201+timiaraines@users.noreply.github.com>
2022-11-14 23:50:32 -0600

    new model

---
+++ flora_fauna/Models/DateValue.swift
@@ -0,0 +1,14 @@
+//
+//  DateValue.swift
+//  flora_fauna
+//
+//  Created by Timia Raines on 11/9/22.
+//
+
+import SwiftUI
+
+struct DateValue: Identifiable{
+    var id = UUID().uuidString
+    var day: Int
+    var date: Date
+}
